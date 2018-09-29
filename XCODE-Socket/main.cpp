b832aafd9d49b380b790002bbfe09b9d15885a41
Brandon Kelm <bkelm816@gmail.com>
2018-09-29 12:57:09 -0400

    Setting up  sockets and binding

--- XCODE-Socket/main.cpp
+++ XCODE-Socket/main.cpp
@@ -12,17 +12,21 @@
 #include <sys/types.h>
 #include <netinet/in.h>
 #include <unistd.h>
+#define PORT 10027
 
 int main(int argc, const char * argv[]) {
     // insert code here...
-    int dPort = 10024;
-    int sPort = 10027;
     int server_fd;
     int opt =1;
-    struct 
-    std::string address = "localhost";
+    int new_socket;
+    int valRead;
+    struct sockaddr_in address;
+    int addrLen = sizeof(address);
+    char buffer[1024] = {0};
     
-    UDPImplement::UDP UDPConnect(dPort, sPort, address );
+    //std::string address = "localhost";
+    
+    //UDPImplement::UDP UDPConnect(dPort, sPort, address );
     server_fd = socket(AF_INET, SOCK_STREAM, 0) ;
     if(server_fd == 0)
     {
@@ -36,6 +40,26 @@ int main(int argc, const char * argv[]) {
         perror("setsockopt");
         exit(EXIT_FAILURE);
     }
+    
+    
+    address.sin_family = AF_INET;
+    address.sin_addr.s_addr = INADDR_ANY;
+    address.sin_port = htons(PORT);
+    
+    
+    if (bind(server_fd, (struct sockaddr *)&address, addrLen) < 0 )
+    {
+        std::cout << "Bind Failed";
+    }
+    if(listen(server_fd, 3)<0)
+    {
+        std::cout << "Listening...";
+    }
+    if ((new_socket = accept(server_fd, (struct sockaddr *)&address, (socklen_t*)&addrLen))<0)
+    {
+        std::cout << "Accept Failed";
+    }
+    valRead = read(new_socket, buffer, 1024);
     //UDPConnect.GetDestPort();
     return 0;
 }
