6e6a8d83668e3dc559cb8abec81815396cc888f6
Brandon Kelm <bkelm816@gmail.com>
2018-09-28 21:28:21 -0400

    Socket implement

--- XCODE-Socket/main.cpp
+++ XCODE-Socket/main.cpp
@@ -8,15 +8,34 @@
 
 #include <iostream>
 #include "Socket.h"
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <netinet/in.h>
+#include <unistd.h>
 
 int main(int argc, const char * argv[]) {
     // insert code here...
     int dPort = 10024;
     int sPort = 10027;
+    int server_fd;
+    int opt =1;
+    struct 
     std::string address = "localhost";
+    
     UDPImplement::UDP UDPConnect(dPort, sPort, address );
+    server_fd = socket(AF_INET, SOCK_STREAM, 0) ;
+    if(server_fd == 0)
+    {
+        std::cout<<"Socket Failure";
+        
+    }
     
-    
+    if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT,
+                   &opt, sizeof(opt)))
+    {
+        perror("setsockopt");
+        exit(EXIT_FAILURE);
+    }
     //UDPConnect.GetDestPort();
     return 0;
 }
